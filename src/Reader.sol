ea2e229839a94deb842c5158da3142f4b171fa22
Joby Thundil <jjtny1@gmail.com>
2025-01-24 17:08:28 -0500

    remove hardcoded storage allowing for variable gas limits

--- src/Reader.sol
+++ src/Reader.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract Reader {
-    uint256[200_000_000] junk;
-
     constructor(uint256 gas_target) {
         uint256 start_gas = gasleft();
         uint256 gas_used = 0;
 
         while (gas_used < gas_target) {
-            junk[gas_used];
+            assembly {
+                let junk := sload(gas_used)
+            }
             gas_used = start_gas - gasleft();
         }
     }
--- src/Writer.sol
+++ src/Writer.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract Writer {
-    uint256[200_000_000] junk;
-
     constructor(uint256 gas_target) {
         uint256 start_gas = gasleft();
         uint256 gas_used = 0;
 
         while (gas_used < gas_target) {
-            junk[gas_used] = gas_used;
+            assembly {
+                sstore(gas_used, gas_used)
+            }
             gas_used = start_gas - gasleft();
         }
     }
